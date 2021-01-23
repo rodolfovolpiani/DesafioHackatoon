234f18dd89577a6a90541465c9e73cb67076811a
Rodolfo Volpiani <rodolfovolpiani@outlook.com>
2021-01-23 14:32:21 -0300

    GET e POST feitas

--- nodejs/cards/src/controllers/card-controller.js
+++ nodejs/cards/src/controllers/card-controller.js
@@ -1,7 +1,41 @@
+const { request, response } = require('express')
 const neDB = require('../configurations/database')
 const api = {}
 
 api.findAll = (request, response) => {
+    neDB.find({}).sort({ name: 1}).exec((exception, cards) => { 
+        if(exception) {
+            const setence = 'Opa, deu ruim na tentativa de listar todos os cards!!!'
+            console.log(setence, exception)
+            response.status(exception.status | 400)
+            response.json({'mensagem' : setence})
+        }
+
+        response.json(cards)
+    })
 }
+
+api.save = (request, response) => {
+    const canonical = request.body
+
+    neDB.insert(canonical, (exception, card) => {
+        if(exception) {
+            console.log('Opa, deu ruim na tentativa de inserir um customer!', exception)
+        }
+        response.json(card)
+        response.status(201)
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = api
--- nodejs/cards/src/routes/card-route.js
+++ nodejs/cards/src/routes/card-route.js
@@ -1,7 +1,8 @@
 const api = require('../controllers/card-controller')
 
 module.exports = (app) => {
-    app.route('')
+    app.route('/cards')
         .get(api.findAll)
+        .post(api.save)
 
 }
